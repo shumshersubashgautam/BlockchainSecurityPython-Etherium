fae0c970742cd1ca8432ec413daff06d4a91120b
Subash Shumsher Gautam <51912776+shumshersubashgautam@users.noreply.github.com>
2023-02-28 18:08:28 +0545

    Add files via upload

--- wallets.py
+++ wallets.py
@@ -8,5 +8,5 @@ web3 = Web3(Web3.HTTPProvider(INFURA))
 print(f'Connected:{web3.isConnected()}')
 
 target_address = web3.toChecksumAddress("0x514910771AF9Ca656af840dff83E8264EcF986CA")
-print(web3.fromWei(web3.eth.getBalance(target_address),'ether'))
-print(web3.eth.get_code(target_address))
+
+print(web3.toHex(web3.eth.get_code(target_address)))
